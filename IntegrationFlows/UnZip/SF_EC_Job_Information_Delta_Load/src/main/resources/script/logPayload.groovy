0da08d408b451be0eab4d359245e8ee70fb81418
Surajit Neogi <surneogi14@gmail.com>
2023-10-14 16:50:44 +0530

    Commit from SAP CI Package - Version 1.0.1

---
+++ IntegrationFlows/UnZip/SF_EC_Job_Information_Delta_Load/src/main/resources/script/logPayload.groovy
@@ -0,0 +1,20 @@
+import com.sap.gateway.ip.core.customdev.util.Message;
+import java.util.HashMap;
+
+def Message processData(Message message) {
+
+	def body = message.getBody(java.lang.String) as String;
+
+	// Log final confirmation payload after mapping
+		def pmap = message.getProperties();
+		String enableLogging = pmap.get("EnablePayloadLogging");
+		
+		if(enableLogging != null && enableLogging.toUpperCase().equals("TRUE")){	
+			def messageLog = messageLogFactory.getMessageLog(message);
+			if(messageLog != null){
+			  messageLog.addAttachmentAsString("Payload", body, "text/xml");
+			}
+		}
+	
+	return message;
+}

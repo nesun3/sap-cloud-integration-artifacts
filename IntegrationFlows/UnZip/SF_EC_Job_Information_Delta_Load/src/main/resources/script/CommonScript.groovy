ad756e284c3c5419b6832fbd653d5a3c0f8c1412
Surajit Neogi <surneogi14@gmail.com>
2023-10-14 16:50:48 +0530

    Commit from SAP CI Package - Version 1.0.1

---
+++ IntegrationFlows/UnZip/SF_EC_Job_Information_Delta_Load/src/main/resources/script/CommonScript.groovy
@@ -0,0 +1,27 @@
+import com.sap.it.api.mapping.*;
+
+/*Add MappingContext parameter to read or set headers and properties
+def String customFunc1(String P1,String P2,MappingContext context) {
+         String value1 = context.getHeader(P1);
+         String value2 = context.getProperty(P2);
+         return value1+value2;
+}
+
+Add Output parameter to assign the output value.
+def void custFunc2(String[] is,String[] ps, Output output, MappingContext context) {
+        String value1 = context.getHeader(is[0]);
+        String value2 = context.getProperty(ps[0]);
+        output.addValue(value1);
+        output.addValue(value2);
+}*/
+
+import com.sap.it.api.mapping.MappingContext;
+
+def String getProperty(String property_name, MappingContext context) {
+
+    String propValue = context.getProperty(property_name);
+
+   return propValue;
+
+}
+

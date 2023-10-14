d7a41bb734290ccc9c34371412baa929abfaec65
Surajit Neogi <surneogi14@gmail.com>
2023-10-14 17:12:31 +0530

    Commit from SAP CI Package - Version 1.0.1

---
+++ IntegrationFlows/UnZip/Zip_Spliting/src/main/resources/script/CreateOrUpdateFileContents.groovy
@@ -0,0 +1,60 @@
+/*
+Refer the link below to learn more about how to Create or update file contents in GitHub.
+https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28#create-or-update-file-contents
+*/
+import com.sap.gateway.ip.core.customdev.util.Message
+import groovy.json.JsonOutput
+
+def Message createFileContents(Message message) {
+    //Headers
+    def prop = message.getProperties()
+    def token = prop.get("GITHUB_ACCESS_TOKEN").toString()
+    def authorization = "Bearer $token"
+
+    message.setHeader("Content-Type", "application/json")
+    message.setHeader("Accept", "application/vnd.github+json")
+    message.setHeader("Authorization", authorization)
+
+    //Body parameters
+    def body = message.getBody(String)
+    def base64Content = body.replaceAll("\\r|\\n", "")
+
+    def commitMessage = prop.get("GITHUB_COMMIT_MESSAGE")
+
+    def jsonOutput = JsonOutput.toJson([
+            message: commitMessage,
+            content: base64Content
+    ])
+
+    println JsonOutput.prettyPrint(jsonOutput)
+    message.setBody(JsonOutput.prettyPrint(jsonOutput))
+    return message
+}
+
+def Message updateFileContents(Message message) {
+    //Headers
+    def prop = message.getProperties()
+    def token = prop.get("GITHUB_ACCESS_TOKEN").toString()
+    def authorization = "Bearer $token"
+
+    message.setHeader("Content-Type", "application/json")
+    message.setHeader("Accept", "application/vnd.github+json")
+    message.setHeader("Authorization", authorization)
+
+    //Body parameters
+    def body = message.getBody(String)
+    def base64Content = body.replaceAll("\\r|\\n", "")
+
+    def commitMessage = prop.get("GITHUB_COMMIT_MESSAGE")
+    def blobSHA
+
+    def jsonOutput = JsonOutput.toJson([
+            message: commitMessage,
+            content: base64Content,
+            sha: blobSHA
+    ])
+
+    println JsonOutput.prettyPrint(jsonOutput)
+    message.setBody(JsonOutput.prettyPrint(jsonOutput))
+    return message
+}

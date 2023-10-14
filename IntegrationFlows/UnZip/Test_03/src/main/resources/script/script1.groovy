91339fc61ab1340bf502a680da209a76ad06b24f
Surajit Neogi <surneogi14@gmail.com>
2023-10-14 17:12:35 +0530

    Commit from SAP CI Package - Version 1.0.9

---
+++ IntegrationFlows/UnZip/Test_03/src/main/resources/script/script1.groovy
@@ -0,0 +1,10 @@
+import com.sap.gateway.ip.core.customdev.util.Message;
+import java.util.HashMap;
+def Message processData(Message message) {
+    
+    def body = message.getBody(String);
+    message.setProperty("property1", "UPADTED");
+    message.setProperty("property2", "NEW");
+    return message;
+    
+}
